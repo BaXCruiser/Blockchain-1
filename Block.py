898a99169a496010e480c22c1b0f840fcf662858
Deepak Kumar <45505692+BaXCruiser@users.noreply.github.com>
2019-04-04 20:30:23 +0530

    Update Block.py

--- Block.py
+++ Block.py
@@ -1,17 +1,18 @@
 import hashlib
 class block:
-  def __init__(self,Sno,time,phash,D):
+  def __init__(self,Sno,pno,time,phash,D):
     self.Sno = Sno
+    self.pno= pno
     self.time = time
     self.phash = phash
     self.D = D
     self.H = self.Hfun()
-    
+
+  def __repr__(self):
+     return "{} - {} - {} - {} - {}".format(self.Sno, self.pno, self.phash, self.time, self.D)
+
   def Hfun(self):
       Var= hashlib.sha256()
-      Var.update(str(self.Sno) + 
-               str(self.time) + 
-               str(self.phash) + 
-               str(self.D))
+      Var.update(str(self.Sno) + str(self.pno) + str(self.time) + str(self.phash) + str(self.D))
       """  hexdigest() :return hexadecimal digits """
       return Var.hexdigest()
