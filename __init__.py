8cf8c979447748051d2c77065752a3e6ddf6aeae
Deepak Kumar <45505692+BaXCruiser@users.noreply.github.com>
2019-04-15 18:44:32 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
+from block import block
+from chain import chain
 
+if __name__=="__init__":        
+    Blockchain=chain()
+    print("Mining about to start ...............")
+    print(Blockchain.getchain())
+   last = Blockchain.current_block
+   last_pno = last.pno
+   pno = Blockchain.proof_of_work(last_pno)
+   Blockchain.get_D(Sender="0",Receiver="pikachu",Amount=1)
+   lastH = last.Hfun
+   Block = Blockchain.make_block(pno, lastH)
+   print("Mining sucessfull")
+   print(Blockchain.chain)
+© 2019 GitHub, Inc.
